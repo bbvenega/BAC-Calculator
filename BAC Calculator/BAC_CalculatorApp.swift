ec02d31a1d3e7ab0a8a9ffac22ab45d7254e5883
Brian <bbvenega@asu.edu>
2025-01-23 15:09:54 -0700

    Initial Commit

---
+++ BAC Calculator/BAC_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BAC_CalculatorApp.swift
+//  BAC Calculator
+//
+//  Created by Brian Venegas on 1/23/25.
+//
+
+import SwiftUI
+
+@main
+struct BAC_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BAC CalculatorTests/BAC_CalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  BAC_CalculatorTests.swift
+//  BAC CalculatorTests
+//
+//  Created by Brian Venegas on 1/23/25.
+//
+
+import Testing
+@testable import BAC_Calculator
+
+struct BAC_CalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
